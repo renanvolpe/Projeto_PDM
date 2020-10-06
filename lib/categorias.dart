f49f44498ce47e7b6e8f8c5b8e10bd98dee6995e
Tiago Freitas <57274846+TiagoFreitas03@users.noreply.github.com>
2020-10-05 22:50:18 -0300

    Alteração na tela de categorias

--- lib/categorias.dart
+++ lib/categorias.dart
@@ -29,27 +29,29 @@ class TelaCategorias extends StatefulWidget {
 class _TelaCategorias extends State<TelaCategorias> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Container(
-            color: Color.fromRGBO(4, 52, 101, 1.0),
-            height: 60,
-            child: Center(
-              child: Text('Categorias', 
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Color.fromRGBO(255, 255, 255, 1.0),
-                  fontSize: 24.0,
-                  fontFamily: 'Aclonica'
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: [
+            Container(
+              color: Color.fromRGBO(4, 52, 101, 1.0),
+              height: 60,
+              child: Center(
+                child: Text('Categorias', 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color.fromRGBO(255, 255, 255, 1.0),
+                    fontSize: 24.0,
+                    fontFamily: 'Aclonica'
+                  ),
                 ),
               ),
             ),
-          ),
-          Categoria(titulo: 'Salgados', icone: FontAwesomeIcons.hamburger, proximaPagina: '\salgados'),
-          Categoria(titulo: 'Doces', icone: FontAwesomeIcons.birthdayCake, proximaPagina: '\doces'),
-          Categoria(titulo: 'Bebidas', icone: FontAwesomeIcons.cocktail, proximaPagina: '\bebidas'),
-        ],
+            Categoria(titulo: 'Salgados', icone: FontAwesomeIcons.hamburger, proximaPagina: '\salgados'),
+            Categoria(titulo: 'Doces', icone: FontAwesomeIcons.birthdayCake, proximaPagina: '\doces'),
+            Categoria(titulo: 'Bebidas', icone: FontAwesomeIcons.cocktail, proximaPagina: '\bebidas'),
+          ],
+        ),
       ),
     );
   }
@@ -71,42 +73,45 @@ class Categoria extends StatefulWidget {
 class _Categoria extends State<Categoria> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Color.fromRGBO(4, 53, 101, 1.0),
-            blurRadius: 3.0,
-            spreadRadius: 2.0,
-            offset: Offset(5.0, 5.0,),
-          )
-        ],
-        color: Colors.white,
-        border: Border.all(color: Color.fromRGBO(4, 53, 101, 1.0), width: 3)
-      ),
-      margin: EdgeInsets.only(left: 20, right: 20, top: 30),
-      child: RaisedButton(
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Icon(widget.icone, size: 120, color: Color.fromRGBO(4, 53, 101, 1.0)),
-              Text(
-                widget.titulo,
-                textAlign: TextAlign.right,
-                style: TextStyle(
-                  color: Color.fromRGBO(4, 53, 101, 1.0),
-                  fontFamily: 'Aclonica', 
-                  fontWeight: FontWeight.bold, 
-                  fontSize: 24.0,
-                ),
-              ),
-            ],
-          ),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(4, 53, 101, 1.0),
+              blurRadius: 3.0,
+              spreadRadius: 2.0,
+              offset: Offset(5.0, 5.0,),
+            )
+          ],
+          color: Colors.white,
+          border: Border.all(color: Color.fromRGBO(4, 53, 101, 1.0), width: 3)
         ),
-        onPressed: () { Navigator.pushNamed(context, '/produtos'); },
+        margin: EdgeInsets.only(left: 20, right: 20, top: 30),
+        child: RaisedButton(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(widget.icone, size: 120, color: Color.fromRGBO(4, 53, 101, 1.0)),
+                Text(
+                  widget.titulo,
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: Color.fromRGBO(4, 53, 101, 1.0),
+                    fontFamily: 'Aclonica', 
+                    fontWeight: FontWeight.bold, 
+                    fontSize: 24.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          onPressed: () { Navigator.pushNamed(context, '/produtos'); },
+        ),
       ),
     );
   }
